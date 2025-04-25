e47c84235faf863ee5aeadec18ac0b58752a1341
Kome1jiSatori <740775405@qq.com>
2025-04-25 10:29:10 +0800

    stock query and grpc

--- internal/order/app/query/service.go
+++ internal/order/app/query/service.go
@@ -3,11 +3,12 @@ package query
 import (
 	"context"
 	"github.com/Kome1jiSatori/gorder-v2/common/genproto/orderpb"
+	"github.com/Kome1jiSatori/gorder-v2/common/genproto/stockpb"
 )
 
 // 定义到stock的通信协议
 type StockService interface {
 	// 检查itemid在不在
-	CheckIfItemsInStock(ctx context.Context, items []*orderpb.ItemWithQuantity) error
+	CheckIfItemsInStock(ctx context.Context, items []*orderpb.ItemWithQuantity) (*stockpb.CheckIfItemsInStockResponse, error)
 	GetItems(ctx context.Context, itemIDs []string) ([]*orderpb.Item, error)
 }
--- internal/stock/ports/grpc.go
+++ internal/stock/ports/grpc.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"github.com/Kome1jiSatori/gorder-v2/common/genproto/stockpb"
 	"github.com/Kome1jiSatori/gorder-v2/stock/app"
-	"github.com/sirupsen/logrus"
+	"github.com/Kome1jiSatori/gorder-v2/stock/app/query"
 )
 
 type GRPCServer struct {
@@ -16,17 +16,20 @@ func NewGRPCServer(app app.Application) *GRPCServer {
 }
 
 func (G GRPCServer) GetItems(ctx context.Context, request *stockpb.GetItemsRequest) (*stockpb.GetItemsResponse, error) {
-	logrus.Info("rpc_request_in, stock.GetItems")
-	defer func() {
-		logrus.Info("rpc_request_out, stock.GetItems")
-	}()
-	return nil, nil
+	items, err := G.app.Queries.GetItems.Handle(ctx, query.GetItems{ItemIDs: request.ItemIDs})
+	if err != nil {
+		return nil, err
+	}
+	return &stockpb.GetItemsResponse{Items: items}, nil
 }
 
 func (G GRPCServer) CheckIfItemsInStock(ctx context.Context, request *stockpb.CheckIfItemsInStockRequest) (*stockpb.CheckIfItemsInStockResponse, error) {
-	logrus.Info("rpc_request_in, stock.CheckIfItemsInStock")
-	defer func() {
-		logrus.Info("rpc_request_out, stock.CheckIfItemsInStock")
-	}()
-	return nil, nil
+	items, err := G.app.Queries.CheckIfItemsInStock.Handle(ctx, query.CheckIfItemsInStock{Items: request.Items})
+	if err != nil {
+		return nil, err
+	}
+	return &stockpb.CheckIfItemsInStockResponse{
+		InStock: 1,
+		Items:   items,
+	}, nil
 }
